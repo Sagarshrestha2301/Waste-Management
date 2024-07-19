b4ed5feb4f563868b67118621cedd04bbfec369b
Sagar Shrestha <sagar23012025@iimscollege.edu.np>
2024-07-19 07:42:14 +0545

    adding

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-=======
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 09c991903e72eb49457a42604e5709115422110e
